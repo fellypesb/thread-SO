36e2b5bcef2a65f315f33d6b562cb4d8ed849c4e
Fellype Siqueira <fellypecsiqueira@gmail.com>
2021-08-26 17:15:23 -0300

    edit 26/08

---
+++ create_process_1.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+	pid_t  pid_parent, pid2;
+
+	pid_parent = getpid();
+	printf("PID Parent: %d\n", pid_parent);	
+
+	pid2 = fork();
+
+	if(pid2==0)
+	{
+		pid2 = fork();
+	}else
+
+	getchar();
+
+	return(0);
+}
+
---
+++ create_process_2.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+	pid_t  pid_parent, pid2;
+	char syscall[11];
+
+	pid_parent = getpid();
+	printf("PID Parent: %d\n", pid_parent);	
+
+	pid2 = fork();
+
+	if(pid2!=0)
+	{
+		pid2 = fork();
+	}
+
+	getchar();
+
+	return(0);
+}
+
